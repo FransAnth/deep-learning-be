a8c7aca5542d2c1131c874e9cc09fd6b27e4aae8
francis <deguzmanfrancis22@gmail.com>
2025-03-24 10:44:44 +0800

    improvement of the neural net

--- DeepLearning/nn_models/neural_network.py
+++ DeepLearning/nn_models/neural_network.py
@@ -5,12 +5,16 @@ class NeuralNetwork(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.hidden_layer = nn.Linear(input_size, hidden_size)
+        self.hidden_layer2 = nn.Linear(hidden_size, hidden_size)
         self.output_layer = nn.Linear(hidden_size, output_size)
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.hidden_layer(x)
+        x = self.relu(x)
+
+        x = self.hidden_layer2(x)
         x = self.relu(x)
 
         x = self.output_layer(x)
