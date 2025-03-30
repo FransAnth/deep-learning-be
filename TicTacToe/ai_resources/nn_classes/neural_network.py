7edbddff8d38a86e2b0b70cad791631e5cad6314
francis <deguzmanfrancis22@gmail.com>
2025-03-30 09:08:52 +0800

    TicTacToe Model Improvement

--- TicTacToe/ai_resources/nn_classes/neural_network.py
+++ TicTacToe/ai_resources/nn_classes/neural_network.py
@@ -2,21 +2,34 @@ import torch.nn as nn
 
 
 class NeuralNetwork(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, activation_func="tanh"):
         super().__init__()
 
         self.hidden_layer = nn.Linear(input_size, hidden_size)
         self.hidden_layer2 = nn.Linear(hidden_size, hidden_size)
         self.output_layer = nn.Linear(hidden_size, output_size)
-        self.tanh = nn.Tanh()
+        if activation_func == "tanh":
+            self.act_func = nn.Tanh()
+        else:
+            self.act_func = nn.LeakyReLU()
+
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.hidden_layer(x)
-        x = self.tanh(x)
+        x = self.act_func(x)
 
         x = self.hidden_layer2(x)
-        x = self.tanh(x)
+        x = self.act_func(x)
+
+        x = self.hidden_layer2(x)
+        x = self.act_func(x)
+
+        x = self.hidden_layer2(x)
+        x = self.act_func(x)
+
+        x = self.hidden_layer2(x)
+        x = self.act_func(x)
 
         x = self.output_layer(x)
         x = self.softmax(x)
--- TicTacToe/views.py
+++ TicTacToe/views.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from django.shortcuts import render
 from rest_framework.response import Response
@@ -7,15 +8,37 @@ from TicTacToe.ai_resources.nn_classes.neural_network import NeuralNetwork
 
 # initialize the model
 input_size = 9
-hidden_size = 135
+hidden_size = 64
 output_size = 9
 
 trained_model = NeuralNetwork(input_size, hidden_size, output_size)
 trained_model.load_state_dict(
     torch.load(r"TicTacToe/ai_resources/trained_ai_models/tic_tac_toe_nn.pth")
 )
-print("model_loaded")
 trained_model.eval()
+
+
+def one_hot_encode(board):
+    """
+    Convert a Tic-Tac-Toe board into a one-hot encoded format.
+
+    Args:
+        board (list or numpy array): 1D list of length 9 with values -1 (O), 0 (empty), 1 (X)
+
+    Returns:
+        torch.Tensor: 1D tensor of length 27 (one-hot encoded board)
+    """
+    encoded_board = np.zeros((9, 3))  # 9 cells, 3 possible values
+
+    for i in range(9):
+        if board[i] == 1:  # X
+            encoded_board[i] = [1, 0, 0]
+        elif board[i] == -1:  # O
+            encoded_board[i] = [0, 1, 0]
+        else:  # Empty
+            encoded_board[i] = [0, 0, 1]
+
+    return encoded_board.flatten()
 
 
 class TicTacToe(APIView):
